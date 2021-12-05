adbcb0db271ba52c2d1c675608ad1babd06287c0
Nick Smedira <nsmedira@me.com>
2021-12-05 12:41:44 -0500

    apply prettier

--- 01/01.js
+++ 01/01.js
@@ -1,33 +1,32 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const countIncreases = (arr) => {
-  let counter = 0;
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[i - 1]) {
-      counter += 1;
+    let counter = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > arr[i - 1]) {
+            counter += 1;
+        }
     }
-  }
-  return counter;
+    return counter;
 };
 
 try {
-  const data = fs.readFileSync("./input.txt", "utf8");
-  const list = data
-    .split("\n")
-    .filter((value) => value)
-    .map((value) => parseInt(value));
+    const data = fs.readFileSync('./input.txt', 'utf8');
+    const list = data
+        .split('\n')
+        .filter((value) => value)
+        .map((value) => parseInt(value));
 
-  console.log("number of individual increases:", countIncreases(list));
+    console.log('number of individual increases:', countIncreases(list));
 
-  counter = 0;
-  const windowSums = [];
-  for (let i = 0; i < list.length; i++) {
-    if (list.length - i >= 3) {
-      windowSums.push(list[i] + list[i + 1] + list[i + 2]);
+    const windowSums = [];
+    for (let i = 0; i < list.length; i++) {
+        if (list.length - i >= 3) {
+            windowSums.push(list[i] + list[i + 1] + list[i + 2]);
+        }
     }
-  }
 
-  console.log("number of windowed increases:", countIncreases(windowSums));
+    console.log('number of windowed increases:', countIncreases(windowSums));
 } catch (error) {
-  console.error(error);
+    console.error(error);
 }
